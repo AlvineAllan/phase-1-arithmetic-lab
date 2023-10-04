392a2e5b072f7af0d089aabc9bb946f729f6df09
Alvine Allan <alvizallaz@gmail.com>
2023-10-04 04:08:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 =31;
+let num2 =2;
+let multiply = num1 * num2;
+
+let random = 10;
+(Math.random() * random);
+
+let num3 = 20;
+let num4 = 5;
+let mod = num3/num4;
+
+let max = 20;
+Math.max(max, 5, 8, 14, 11);
